7ed89a99b0dede6e7396ac490ad336fda71d7364
Kyle Rakos <krakos@purdue.edu>
2017-02-07 18:08:02 -0500

    Initial pipeline

--- source/caches.sv
+++ source/caches.sv
@@ -16,12 +16,8 @@
 module caches (
   input logic CLK, nRST,
   datapath_cache_if.cache dcif,
-  caches_if.caches cif
+  caches_if cif
 );
-  // import types
-  import cpu_types_pkg::word_t;
-
-  parameter CPUID = 0;
 
   word_t instr;
   word_t daddr;
@@ -49,10 +45,17 @@ module caches (
   // dcache invalidate before halt
   assign dcif.flushed = dcif.halt;
 
+<<<<<<< HEAD
   //single cycle
   assign dcif.ihit = (dcif.imemREN) ? ~cif.iwait : 0;
   assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~cif.dwait : 0;
   assign dcif.imemload = (cif.iwait) ? instr : cif.iload;
+=======
+  //singlecycle
+  assign dcif.ihit = (dcif.imemREN) ? ~cif.iwait : 0;
+  assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~cif.dwait : 0;
+  assign dcif.imemload = cif.iload;
+>>>>>>> 7fcc3ae4a585e8f1874cfc6210eb1fc452df4572
   assign dcif.dmemload = cif.dload;
 
 
@@ -61,6 +64,10 @@ module caches (
   assign cif.dWEN = dcif.dmemWEN;
   assign cif.dstore = dcif.dmemstore;
   assign cif.iaddr = dcif.imemaddr;
+<<<<<<< HEAD
   assign cif.daddr = daddr;
+=======
+  assign cif.daddr = dcif.dmemaddr;
+>>>>>>> 7fcc3ae4a585e8f1874cfc6210eb1fc452df4572
 
 endmodule
