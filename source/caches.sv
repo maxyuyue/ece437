b9bc02bae30c18c269cf6673b3a09bc10a2794c9
Kyle Rakos <krakos@purdue.edu>
2017-02-07 18:16:32 -0500

    Fixed merge conflicts

--- source/caches.sv
+++ source/caches.sv
@@ -43,19 +43,13 @@ module caches (
     end
   end
   // dcache invalidate before halt
-  assign dcif.flushed = dcif.halt;
+  assign dcif.flushed = dcif.halt; 
 
-<<<<<<< HEAD
-  //single cycle
-  assign dcif.ihit = (dcif.imemREN) ? ~cif.iwait : 0;
-  assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~cif.dwait : 0;
-  assign dcif.imemload = (cif.iwait) ? instr : cif.iload;
-=======
+
   //singlecycle
   assign dcif.ihit = (dcif.imemREN) ? ~cif.iwait : 0;
   assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~cif.dwait : 0;
   assign dcif.imemload = cif.iload;
->>>>>>> 7fcc3ae4a585e8f1874cfc6210eb1fc452df4572
   assign dcif.dmemload = cif.dload;
 
 
@@ -64,10 +58,8 @@ module caches (
   assign cif.dWEN = dcif.dmemWEN;
   assign cif.dstore = dcif.dmemstore;
   assign cif.iaddr = dcif.imemaddr;
-<<<<<<< HEAD
-  assign cif.daddr = daddr;
-=======
+
   assign cif.daddr = dcif.dmemaddr;
->>>>>>> 7fcc3ae4a585e8f1874cfc6210eb1fc452df4572
+
 
 endmodule
