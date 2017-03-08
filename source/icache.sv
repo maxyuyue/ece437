4e11b3f44ddab0f6f0cb57755af0b4a07dc97c72
Kshitij Jain <jain98@purdue.edu>
2017-03-08 14:08:48 -0500

    Icache block diagram and updated icache .sv

--- source/icache.sv
+++ source/icache.sv
@@ -6,7 +6,7 @@
 // cpu types
 `include "cpu_types_pkg.vh"
 
-module caches (
+module icache (
   input logic CLK, nRST,
   datapath_cache_if.cache dcif,
   caches_if.caches cif
@@ -14,28 +14,72 @@ module caches (
   // import types
   import cpu_types_pkg::word_t;
 
-  logic Hit;
+  logic[31:0] instr, new_instr;
+  icachef_t query;
+  logic isHit, isValid;
 
-  typedef enum bit {MISS,IDLE} state_t;
-  state_t state, nxtstate;
+  //icache
+  icachef_t[15:0] info; //tag, index and byte offset
+  logic[15:0] valid_bit;  //valid bit for each slot
+  logic[15:0][31:0] data; //data in each slot
 
+
+  //query initializations
+  assign query.tag = dcif.imemaddr[31:6];
+  assign query.idx = dcif.imemaddr[5:2];
+  assign isHit = info[query.idx].tag == query.tag;
+  assign isValid = valid_bit[query.idx];
+
+  integer i;
 
   always_ff @(posedge CLK, negedge nRST)
   	begin
-
+      if(!nRST)
+        begin
+          instr <= 32b'0;
+          for(i = 0; i < 16; i++)
+            begin
+              valid_bit[i] <= 0;
+            end
+        end
+      else
+        begin
+          instr <= new_instr;
+        end
  		end
 
   always_comb
 		begin
-			case(state)
-				IDLE:
-					begin
+			new_instr = instr;
+      dcif.ihit = 0;
+      dcif.imemload = 0;
+      cif.iREN = 0;
+      cif.iaddr = 0;
 
-					end
-
-				MISS:
-					begin
-
-					end
-			endcase // state
+      if(isValid && isHit) //Query found in cache
+        begin
+          dcif.imemload = data[query.idx];
+          dcif.ihit = 1;
+          new_instr = data[query.idx];
+        end
+      else  //fetch value from memory
+        begin
+          cif.iREN = dcif.imemREN;
+          cif.iaddr = dcif.imemaddr;
+          if(~cif.iwait)
+            begin
+              valid_bit[query.idx] = 1;
+              data[query.idx] = cif.iload;
+              info[query.idx].tag = query.tag;
+              info[query.idx].idx = query.idx;
+              dcif.ihit = 1;
+              dcif.imemload = cif.iload;
+              new_instr = cif.iload;
+            end
+          else
+            begin
+              dcif.imemload = instr;
+            end
+        end
 		end
+  endmodule // icache
