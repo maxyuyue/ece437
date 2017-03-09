c5e482486f41f3b2f6f0652912ad8efd094bd824
Kshitij Jain <jain98@purdue.edu>
2017-03-09 15:38:12 -0500

    icache code and testbench

--- source/caches.sv
+++ source/caches.sv
@@ -15,32 +15,30 @@
 
 module caches (
   input logic CLK, nRST,
-  datapath_cache_if.cache dcif,
-  caches_if.caches cif
+  datapath_cache_if dcif,
+  caches_if cif
 );
-  /*
   // icache
   icache  ICACHE(CLK, nRST, dcif, cif);
   // dcache
-  dcache  DCACHE(CLK, nRST, dcif, cif);
-  */
-  
-   // dcache invalidate before halt handled by dcache when exists
-  assign dcif.flushed = dcif.halt;
+  //dcache  DCACHE(CLK, nRST, dcif, cif);
+    
+  //  // dcache invalidate before halt handled by dcache when exists
+  // assign dcif.flushed = dcif.halt;
 
-  //singlecycle
-  assign dcif.ihit = (dcif.imemREN) ? ~cif.iwait : 0;
-  assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~cif.dwait : 0;
-  assign dcif.imemload = cif.iload;
-  assign dcif.dmemload = cif.dload;
+  // //singlecycle
+  // assign dcif.ihit = (dcif.imemREN) ? ~cif.iwait : 0;
+  // assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~cif.dwait : 0;
+  // assign dcif.imemload = cif.iload;
+  // assign dcif.dmemload = cif.dload;
 
 
-  assign cif.iREN = dcif.imemREN;
-  assign cif.dREN = dcif.dmemREN;
-  assign cif.dWEN = dcif.dmemWEN;
-  assign cif.dstore = dcif.dmemstore;
-  assign cif.iaddr = dcif.imemaddr;
-  assign cif.daddr = dcif.dmemaddr;
+  // assign cif.iREN = dcif.imemREN;
+  // assign cif.dREN = dcif.dmemREN;
+  // assign cif.dWEN = dcif.dmemWEN;
+  // assign cif.dstore = dcif.dmemstore;
+  // assign cif.iaddr = dcif.imemaddr;
+  // assign cif.daddr = dcif.dmemaddr;
   
 
 endmodule
