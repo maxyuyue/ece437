517c928b950269c2b3fb9281f32ba044cdf2c9fd
Kyle Rakos <krakos@purdue.edu>
2017-03-08 21:02:07 -0500

    Cleaned up some gross code from pipelining. No functionality change.

--- source/caches.sv
+++ source/caches.sv
@@ -18,49 +18,29 @@ module caches (
   datapath_cache_if.cache dcif,
   caches_if.caches cif
 );
-  // import types
-  // import cpu_types_pkg::word_t;
-
-  // parameter CPUID = 0;
-
-  // word_t instr;
-  // word_t daddr;
-
+  
   // icache
   icache  ICACHE(CLK, nRST, dcif, cif);
   // dcache
   dcache  DCACHE(CLK, nRST, dcif, cif);
 
-  // single cycle instr saver (for memory ops)
-  // always_ff @(posedge CLK)
-  // begin
-  //   if (!nRST)
-  //   begin
-  //     instr <= '0;
-  //     daddr <= '0;
-  //   end
-  //   else
-  //   if (dcif.ihit)
-  //   begin
-  //     instr <= cif.iload;
-  //     daddr <= dcif.dmemaddr;
-  //   end
-  // end
-  // // dcache invalidate before halt
-  // assign dcif.flushed = dcif.halt;
+  /*
+   // dcache invalidate before halt handled by dcache when exists
+  assign dcif.flushed = dcif.halt;
 
-  // //single cycle
-  // assign dcif.ihit = (dcif.imemREN) ? ~cif.iwait : 0;
-  // assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~cif.dwait : 0;
-  // assign dcif.imemload = (cif.iwait) ? instr : cif.iload;
-  // assign dcif.dmemload = cif.dload;
+  //singlecycle
+  assign dcif.ihit = (dcif.imemREN) ? ~cif.iwait : 0;
+  assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~cif.dwait : 0;
+  assign dcif.imemload = cif.iload;
+  assign dcif.dmemload = cif.dload;
 
 
-  // assign cif.iREN = dcif.imemREN;
-  // assign cif.dREN = dcif.dmemREN;
-  // assign cif.dWEN = dcif.dmemWEN;
-  // assign cif.dstore = dcif.dmemstore;
-  // assign cif.iaddr = dcif.imemaddr;
-  // assign cif.daddr = daddr;
+  assign cif.iREN = dcif.imemREN;
+  assign cif.dREN = dcif.dmemREN;
+  assign cif.dWEN = dcif.dmemWEN;
+  assign cif.dstore = dcif.dmemstore;
+  assign cif.iaddr = dcif.imemaddr;
+  assign cif.daddr = dcif.dmemaddr;
+  */
 
 endmodule
