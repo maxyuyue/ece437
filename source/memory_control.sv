a4446412dbc9d3233a16ee65ce72da90cec256c3
Kyle Rakos <krakos@purdue.edu>
2017-03-29 12:10:30 -0400

    Fixed some files the course pull overwrote

--- source/memory_control.sv
+++ source/memory_control.sv
@@ -22,4 +22,83 @@ module memory_control (
   // number of cpus for cc
   parameter CPUS = 2;
 
+  /*
+    ccif.iwait: 1 when no instruction to load and/or not access busy, 0 otherwise ->
+    ccif.dwait: 1 when no data to load/write and/or not access busy, 0 otherwise ->
+    ccif.iREN: 1 when ask to read(load) instruction, 0 otherwise <-
+    ccif.dREN: 1 when ask to read(load) data, 0 otherwise <-
+    ccif.dWEN: 1 when ready to write data, 0 otherwise <-
+    ccif.iload: instruction to return to cache (32) ->
+    ccif.dload: data to return to cache (32) ->
+    ccif.dstore: data to write to RAM (32) <-
+    ccif.iaddr: address to load instruction from (32) <-
+    ccif.daddr: address to write/load data from (32) <- 
+
+    ccif.ramWEN: 1 when requesting to write data, 0 otherwise ->
+    ccif.ramREN: 1 when requesting to read(load) instruction or data, 0 otherwise ->
+    ccif.ramstate: Indicates current state of RAM as either FREE, BUSY, ACCESS, or ERROR <-
+    ccif.ramaddr: Address to load/write from (32) ->
+    ccif.ramstore: Data to write (32) ->
+    ccif.ramload: Data returned from RAM (32) <-
+
+  */
+
+  always_comb // determine wait signals sent to cache
+  begin
+    if (ccif.ramstate == ACCESS) begin
+      if (ccif.dREN == 1'b1) begin // data ready to be read
+        ccif.iwait = 1'b1;
+        ccif.dwait = 1'b0;
+      end
+      else if (ccif.dWEN == 1'b1) begin // data ready to be written
+        ccif.iwait = 1'b1;
+        ccif.dwait = 1'b0;
+      end
+      else if (ccif.iREN == 1'b1) begin // instruction ready to be read
+        ccif.iwait = 1'b0;
+        ccif.dwait = 1'b1;
+      end
+      else begin //default is data is not ready
+        ccif.iwait = 1'b1;
+        ccif.dwait = 1'b1;
+      end
+      end
+    else begin //default is data is not ready
+      ccif.iwait = 1'b1;
+      ccif.dwait = 1'b1;
+    end
+  end
+
+
+
+  always_comb begin // determine RAM signals
+    
+    if (ccif.dREN == 1'b1) begin // ready to read data from RAM
+      ccif.ramWEN = 1'b0;
+      ccif.ramREN = 1'b1;
+      ccif.ramaddr = ccif.daddr; 
+    end
+    else if (ccif.dWEN == 1'b1) begin // ready to write data to RAM
+      ccif.ramWEN = 1'b1;
+      ccif.ramREN = 1'b0;
+      ccif.ramaddr = ccif.daddr;
+    end
+    else if (ccif.iREN == 1'b1) begin // ready to read instruction from RAM
+      ccif.ramWEN = 1'b0;
+      ccif.ramREN = 1'b1;
+      ccif.ramaddr = ccif.iaddr;
+    end
+    else begin // default don't access memory
+      ccif.ramWEN = 1'b0;
+      ccif.ramREN = 1'b0;
+      ccif.ramaddr = 32'hBAD1BAD1;
+    end
+
+    ccif.dload = ccif.ramload;
+    ccif.iload = ccif.ramload;
+    ccif.ramstore = ccif.dstore; // Data to store
+  
+    
+  end
+
 endmodule
--- testbench/register_file_tb.sv
+++ testbench/register_file_tb.sv
@@ -1,6 +1,8 @@
 /*
-  Eric Villasenor
-  evillase@gmail.com
+  Kyle Rakos
+  krakos@purdue.edu
+
+  register_file_tb.sv
 
   register file test bench
 */
@@ -28,11 +30,11 @@ module register_file_tb;
   // interface
   register_file_if rfif ();
   // test program
-  test PROG ();
+  test PROG (CLK,rfif,nRST);
   // DUT
-`ifndef MAPPED
+  `ifndef MAPPED
   register_file DUT(CLK, nRST, rfif);
-`else
+  `else
   register_file DUT(
     .\rfif.rdat2 (rfif.rdat2),
     .\rfif.rdat1 (rfif.rdat1),
@@ -43,10 +45,93 @@ module register_file_tb;
     .\rfif.WEN (rfif.WEN),
     .\nRST (nRST),
     .\CLK (CLK)
-  );
-`endif
+    );
+  `endif
 
 endmodule
 
-program test;
+program test (
+  input logic  CLK,
+  register_file_if.rf rfif,
+  output logic nRST
+  );
+    parameter PERIOD = 10;
+
+    initial begin
+    // Initialization
+    nRST = 0;
+    rfif.wsel = 5'h00;
+    rfif.wdat = 32'h00000000;
+    rfif.WEN = 0;
+    rfif.rsel1 = 5'h0;
+    rfif.rsel2 = 5'h0; 
+    @(posedge CLK); @(posedge CLK); @(posedge CLK); @(posedge CLK);
+
+    // Write value to reg 1
+    nRST = 1;
+    rfif.wsel = 5'h01;
+    rfif.wdat = 32'hfedcba98;
+    rfif.WEN = 1;
+    rfif.rsel1 = 5'h0;
+    @(posedge CLK); @(posedge CLK); @(posedge CLK); @(posedge CLK);
+
+    // Write value to reg 31
+    nRST = 1;
+    rfif.wsel = 5'd31;
+    rfif.wdat = 32'h89abcdef;
+    rfif.WEN = 1;
+    rfif.rsel1 = 5'h0;
+    rfif.rsel2 = 5'd31;
+    @(posedge CLK); @(posedge CLK); @(posedge CLK); @(posedge CLK);
+
+
+  // Write value to reg 25 without write enable
+    nRST = 1;
+    rfif.wsel = 5'd25;
+    rfif.wdat = 32'hbbbbbbbb;
+    rfif.WEN = 0;
+    rfif.rsel1 = 5'd25;
+    rfif.rsel2 = 5'd31;
+    @(posedge CLK); @(posedge CLK); @(posedge CLK); @(posedge CLK);
+
+    // Write value to reg 25 with write enable
+    nRST = 1;
+    rfif.wsel = 5'd25;
+    rfif.wdat = 32'hbbbbbbbb;
+    rfif.WEN = 1;
+    rfif.rsel1 = 5'd25;
+    rfif.rsel2 = 5'd31;
+    @(posedge CLK); @(posedge CLK); @(posedge CLK); @(posedge CLK);
+
+    // Write value to reg 24 without initially showing it
+    nRST = 1;
+    rfif.wsel = 5'd24;
+    rfif.wdat = 32'hcccccccc;
+    rfif.WEN = 1;
+    rfif.rsel1 = 5'd25;
+    rfif.rsel2 = 5'd31;
+    @(posedge CLK); @(posedge CLK); @(posedge CLK); @(posedge CLK);
+
+  // Show reg 24
+     nRST = 1;
+    rfif.wsel = 5'd24;
+    rfif.wdat = 32'h00000000;
+    rfif.WEN = 0;
+    rfif.rsel1 = 5'd24;
+    rfif.rsel2 = 5'd31;
+    @(posedge CLK); @(posedge CLK); @(posedge CLK); @(posedge CLK);
+
+  // Reset
+    nRST = 0;
+    rfif.wsel = 5'd00;
+    rfif.wdat = 32'haaaaaaaa;
+    rfif.WEN = 0;
+    rfif.rsel1 = 5'd24;
+    rfif.rsel2 = 5'd31;
+    @(posedge CLK); @(posedge CLK); @(posedge CLK); @(posedge CLK);
+
+
+    end
+
+
 endprogram
